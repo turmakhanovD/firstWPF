9ae14e0400970e0941c56fb0abf1c3a11a2bb90c
Турмахан Динислам Даулетулы <@corp.itstep>
2019-04-29 17:35:11 +0600

    First Wpf lesson hw

---
+++ FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Student student = new Student
+            {
+                Name = "Islam",
+                Surname = "Turmakhan",
+                Age = 16,
+                Group = "SDP-181"
+            };
+
+            nameLabel.Content = student.Name;
+            surnameLabel.Content = student.Surname;
+            ageLabel.Content = student.Age;
+            groupLabel.Content = student.Group;
+        }
+    }
+}
---
+++ FirstWPF/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstWPF
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+    }
+}
